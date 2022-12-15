c6ced480e368eb29b1c0605ed908217f449bad5e
Brendan Wang <byw2@princeton.edu>
2022-12-15 16:58:04 -0500

    ambulance refactoring

--- COS-426-Final-Project/src/components/objects/Ambulance/Ambulance.js
+++ COS-426-Final-Project/src/components/objects/Ambulance/Ambulance.js
@@ -1,4 +1,4 @@
-import { Group, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { Group } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import MODEL from './scene.gltf';
 
@@ -7,7 +7,8 @@ class Ambulance extends Group {
         // Call parent Group() constructor
         super();
         this.name = 'ambulance';
-        // adapted from https://discourse.threejs.org/t/three-js-simple-jump/40411
+        // Jumping logic (acceleration factor) background taken from
+        // https://discourse.threejs.org/t/three-js-simple-jump/40411
         this.state = {
             onGround: true,
             velocity_y: 0,
@@ -16,24 +17,13 @@ class Ambulance extends Group {
 
         const loader = new GLTFLoader();
 
-        // https://github.com/harveyw24/Glider
-        // 
+        // Adapted from: https://github.com/harveyw24/Glider
         this.name = 'ambulance';
         loader.load(MODEL, (gltf) => {
             gltf.scene.scale.set(0.008, 0.008, 0.008);
             // gltf.scene.children[0].position.set(0, -0.25, 0);
             this.add(gltf.scene);
         });
-
-        // const geometry = new BoxGeometry( 1.5, 1.5, 1.5 );
-        // const material = new MeshBasicMaterial( {color: 0xf0f0f0} );
-
-        // const cube = new Mesh( geometry, material );
-        // // set cube on the road
-        // cube.position.set( 0, -0.25, 0 );
-
-        // this.add(cube);
-
     }
 }
 
